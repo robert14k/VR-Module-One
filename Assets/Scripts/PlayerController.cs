a4b81181aa3e39f3b7f1b7bf3cf66e93720c8ef1
mitchellguynan <78188107+mitchellguynan@users.noreply.github.com>
2021-02-01 16:28:31 -0600

    BumpMaps/Ship Inclusion
    Applied bump maps to moon and floor, included ships

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,7 +42,7 @@ public class PlayerController : MonoBehaviour
     void setCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 17)
+        if(count >= 16)
         {
             winText.text = "You Win!";
         }
